dc1e00e4ef833ff13e88e0eb812f423c59cf193f
giuseppe <giuseppe.ranieri@outlook.it>
2020-09-08 12:11:24 +0200

    publishing first

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Oscillator : MonoBehaviour
+{
+    [Range(0, 1)][SerializeField] float movementFactor ;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+    Vector3 startingPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon){ return; }
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawsinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawsinWave / 2f + 0.5f;
+
+        Vector3 offset = movementFactor*movementVector;
+        transform.position = startingPos + offset;
+    }
+}
---
+++ Assets/Rocket.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+    [SerializeField] float mainThrust = 25f;
+    [SerializeField] float rcsThrust = 25f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deadSound;
+    [SerializeField] AudioClip winSound;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deadParticles;
+    [SerializeField] ParticleSystem winParticles;
+
+    enum State { Alive,Dying,Transcending }
+    State state = State.Alive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // todo stop sound on dead
+        if(state == State.Alive)
+        {
+            RespondToRotate();
+            RespondToThrust();
+        }
+        
+    }
+
+    private void RespondToRotate()
+    {
+        rigidbody.freezeRotation = true;
+
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward* rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+        rigidbody.freezeRotation = false;
+    }
+
+    private void RespondToThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidbody.AddRelativeForce(Vector3.up * rcsThrust);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Respawn":
+                //do nothing at the moment
+                break;
+            case "Finish":
+                StartSuccesSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deadSound);
+        deadParticles.Play();
+        Invoke("LoadFirstLevel", 1f); //parameterise
+    }
+
+    private void StartSuccesSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(winSound);
+        winParticles.Play();
+        Invoke("LoadNextLevel", 1f); //parameterise
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
